f4ce4b9fd1da8719237de1521e299a56b3f87f45
Skyl3Z <sabagogichaishvili15@gmail.com>
2024-12-13 22:14:38 +0400

    fifth js task

---
+++ index.js
@@ -0,0 +1,56 @@
+// for(let i = 150; i>=0 ; i-- ) {
+//     console.log(i) ;
+// }
+
+// for(let i = 150; i>=0 ; i -= 2 ) {
+//     console.log(i) ;
+// }
+
+// for(let i = 150; i>=0 ; i -= 3 ) {
+//     console.log(i) ;
+// }
+
+// for(let i = 0; i<=150 ; i += 2 ) {
+//     console.log(i) ;
+// }
+
+// for(let i = 0; i<=150 ; i += 3 ) {
+//     console.log(i) ;
+// }
+
+// for(let i = 0; i<=150 ; i += 4 ) {
+//     console.log(i) ;
+// }
+
+
+
+
+let i = 150 ;
+
+// while(i>=0){
+//     console.log(i--)
+// }
+
+// while(i>0){
+//     console.log(i-=2)
+// }
+
+// while(i>0){
+//     console.log(i-=3)
+// }
+
+let x = 0 ;
+
+
+// while(x<150){
+    //     console.log(x +=2)
+    // }
+    
+// while(x<150) {
+//     console.log(x +=3)
+// }
+
+// while(x<150){
+//     console.log(x +=4)
+// }
+
